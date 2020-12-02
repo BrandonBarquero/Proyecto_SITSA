f7c527b23d6e519b97e426542d31efbb7ce8e779
BrandonBarquero <brandon.barquero.berrocal@est.una.ac.cr>
2020-12-02 16:54:47 -0600

    Cierre Mes Modelo

--- Sitsa_Proyecto/Controllers/Cierre_MesController.cs
+++ Sitsa_Proyecto/Controllers/Cierre_MesController.cs
@@ -1,4 +1,5 @@
 ﻿using Biblioteca_Clases.DAO;
+using Biblioteca_Clases.Modelos;
 using Biblioteca_Clases.Models;
 using Biblioteca_Clases.Seguridad;
 using Newtonsoft.Json;
@@ -19,6 +20,8 @@ namespace Sitsa_Proyecto.Controllers
         Cierre_MesDAO dao_cierre = new Cierre_MesDAO();
         Mail mail = new Mail();
         Encryption encryption = new Encryption();
+
+        Cierre_MesModelo cierre_modelo = new Cierre_MesModelo();
         // GET: Cierre_Mes
         public ActionResult Index()
         {
@@ -30,13 +33,7 @@ namespace Sitsa_Proyecto.Controllers
 
             string perfil = (string)(Session["User"]);
 
-            bool permiso = dao.PERMISO_APROBAR(perfil);
-            int permisos = 0;
-            if (permiso == true)
-            {
-                permisos = 1;
-            }
-
+            int permisos = cierre_modelo.Permisos(perfil);
 
             return Json(permisos, JsonRequestBehavior.AllowGet);
         }
@@ -45,13 +42,7 @@ namespace Sitsa_Proyecto.Controllers
 
             string perfil = (string)(Session["User"]);
 
-            bool permiso = dao.PERMISO_APROBAR_R(perfil);
-            int permisos = 0;
-            if (permiso == true)
-            {
-                permisos = 1;
-            }
-
+            int permisos = cierre_modelo.Permisos_Reenvio(perfil);
 
             return Json(permisos, JsonRequestBehavior.AllowGet);
         }
@@ -60,175 +51,56 @@ namespace Sitsa_Proyecto.Controllers
         public JsonResult AceptarReporte(string dato)
         {
 
-            int fk_id_reporte = int.Parse(dato);
-
             string user = (string)(Session["User"]);
 
-            dao_cierre.AgregarCierreMes(fk_id_reporte, user, fecha.fecha());
-
-
-            int result = daoreporte.aceptarreporte(dato, user, fecha.fecha());
-
-
+            int result = cierre_modelo.AceptarReporte(dato, user);
 
             return Json(result, JsonRequestBehavior.AllowGet);
         }
         public JsonResult RechazarReporte(string dato)
         {
-            int fk_id_reporte = int.Parse(dato);
 
             string user = (string)(Session["User"]);
 
-            dao_cierre.Eliminar_Cierre_Mes(fk_id_reporte);
-
-            int result = daoreporte.RechazarReporte(dato, user, fecha.fecha());
-
-
+            int result = cierre_modelo.RechazarReporte(dato, user);
 
             return Json(result, JsonRequestBehavior.AllowGet);
         }
         public JsonResult Facturados()
         {
+            string sJSONResponse = cierre_modelo.Facturados();
 
-            List<Reporte> list = daoreporte.listarfacturados();
-
-
-            string sJSONResponse = JsonConvert.SerializeObject(list, Formatting.Indented);
             return Json(sJSONResponse, JsonRequestBehavior.AllowGet);
-
-
-
         }
         public JsonResult Buscar(string reporte, string cliente, string horas_convertidas, string horas_convertidas2)
         {
-
-            if (horas_convertidas.Equals("") && horas_convertidas2.Equals(""))
-            {
-                horas_convertidas = "1999-10-10";
-                horas_convertidas2 = "2040-10-10";
-            }
-            if (horas_convertidas2.Equals(""))
-            {
-                horas_convertidas = "1999-10-10";
-                horas_convertidas2 = "2040-10-10";
-            }
-            if (horas_convertidas.Equals(""))
-            {
-                horas_convertidas = "1999-10-10";
-                horas_convertidas2 = "2040-10-10";
-            }
-
-
-
-
-            List<Reporte> list = daoreporte.listaReporte(reporte, cliente, horas_convertidas, horas_convertidas2);
-
-
-            string sJSONResponse = JsonConvert.SerializeObject(list, Formatting.Indented);
+            string sJSONResponse = cierre_modelo.Buscar(reporte, cliente, horas_convertidas, horas_convertidas2);
 
             return Json(sJSONResponse, JsonRequestBehavior.AllowGet);
-
         }
         public JsonResult Buscar_Facturados(string reporte, string cliente, string horas_convertidas, string horas_convertidas2)
         {
-
-            if (horas_convertidas.Equals("") && horas_convertidas2.Equals(""))
-            {
-                horas_convertidas = "1999-10-10";
-                horas_convertidas2 = "2040-10-10";
-            }
-            if (horas_convertidas2.Equals(""))
-            {
-                horas_convertidas = "1999-10-10";
-                horas_convertidas2 = "2040-10-10";
-            }
-            if (horas_convertidas.Equals(""))
-            {
-                horas_convertidas = "1999-10-10";
-                horas_convertidas2 = "2040-10-10";
-            }
-
-
-
-
-            List<Reporte> list = daoreporte.listaReportefacturados(reporte, cliente, horas_convertidas, horas_convertidas2);
-
-
-            string sJSONResponse = JsonConvert.SerializeObject(list, Formatting.Indented);
+            string sJSONResponse = cierre_modelo.Buscar_Facturados(reporte, cliente, horas_convertidas, horas_convertidas2);
 
             return Json(sJSONResponse, JsonRequestBehavior.AllowGet);
-
         }
         public JsonResult Generales()
         {
+            string sJSONResponse = cierre_modelo.Generales();
 
-            List<Reporte> list = daoreporte.listar();
-
-
-            string sJSONResponse = JsonConvert.SerializeObject(list, Formatting.Indented);
             return Json(sJSONResponse, JsonRequestBehavior.AllowGet);
-
-
-
         }
 
         public JsonResult Reenviar_Correo(string dato)
         {
+            string sJSONResponse = cierre_modelo.Reenviar_Correo(dato);
 
-            List<Contacto> list = dao_cierre.listaCorreos(dato);
-
-
-            string sJSONResponse = JsonConvert.SerializeObject(list, Formatting.Indented);
             return Json(sJSONResponse, JsonRequestBehavior.AllowGet);
-
-
-
         }
 
         public JsonResult Cambiar_Estado_Reporte(string ID_Reporte, string correos)
         {
-
-            int ID_Reporte2 = int.Parse(ID_Reporte);
-
-            string nombre_cliente = daoreporte.ObtenerNombreCliente2(ID_Reporte2);
-
-            string[] vector_correo = correos.Split(',');
-
-            int result = dao_cierre.Cambiar_Estado_Reenvio(ID_Reporte2);
-
-
-            Reporte Reporte_Obj = new Reporte();
-
-            Reporte_Obj = daoreporte.devuelve_reporte(ID_Reporte2);
-
-            Detalle_Reporte Detalle_Obj2 = new Detalle_Reporte();
-
-            int opc = 0;
-            if (Reporte_Obj.ID_CONTRATO != 0)
-            {
-                opc = 1;
-            }
-            else if (Reporte_Obj.ID_PROYECTO != 0)
-            {
-                opc = 2;
-            }
-
-
-
-            List<Detalle_Reporte> Detalle_Obj = new List<Detalle_Reporte>();
-
-            Detalle_Obj = daoreporte.BuscaDetallesReporte(ID_Reporte2, opc);
-
-
-            if ((Reporte_Obj.TIPO_DOCUMENTO == "Reporte Contrato") || (Reporte_Obj.TIPO_DOCUMENTO == "Reporte Contrato Garantía") || (Reporte_Obj.TIPO_DOCUMENTO == "Reporte Contrato Facturado"))
-            {
-                mail.Enviar_Resporte_Correo(encryption.Encrypt(ID_Reporte2.ToString()), Reporte_Obj, Detalle_Obj, nombre_cliente, vector_correo);
-            }
-            else
-            {
-                mail.Enviar_Resporte_Correo_Proyecto(encryption.Encrypt(ID_Reporte2.ToString()), Reporte_Obj, Detalle_Obj2, nombre_cliente, vector_correo);
-            }
-
+            int result = cierre_modelo.Cambiar_Estado_Reporte(ID_Reporte, correos);
 
             return Json(result, JsonRequestBehavior.AllowGet);
 
