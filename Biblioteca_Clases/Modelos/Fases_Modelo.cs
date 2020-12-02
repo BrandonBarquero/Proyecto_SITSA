d351b4557a87e08e29a74734f255cbafa59adb9f
BrandonBarquero <brandon.barquero.berrocal@est.una.ac.cr>
2020-12-01 21:48:38 -0600

    fase tiempo modelo

---
+++ Biblioteca_Clases/Modelos/Fases_Modelo.cs
@@ -0,0 +1,69 @@
+﻿using Biblioteca_Clases.DAO;
+using Biblioteca_Clases.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Clases.Modelos
+{
+   public class Fases_Modelo
+    {
+
+        Fase_TiempoDAO dao_fase = new Fase_TiempoDAO();
+
+
+        public string agregar_fase(Fase_Tiempo fase, string user)
+        {
+            var t = fase;
+            string validacion = "fail";
+            Fecha fecha = new Fecha();
+
+            Fase_Tiempo fase_tiempo = new Fase_Tiempo();
+            fase_tiempo.DESCRIPCION = t.DESCRIPCION;
+            fase_tiempo.TIEMPO = t.TIEMPO;
+            fase_tiempo.FECHA_CREACION = fecha.fecha();
+            fase_tiempo.USUARIO_CREACION = user;
+            fase_tiempo.FK_ID_PROYECTO = t.FK_ID_PROYECTO;
+
+            //int result = dao_fase.AgregarFase_Tiempo(fase_tiempo);
+
+            //if (result == 1)
+            //{
+            //    validacion = "sucess";
+            //}
+            return validacion;
+        }
+        public string actualizar_estado_deshabilitar_fase_tiempo(Fase_Tiempo fase, string user)
+        {
+            var t = fase;
+            string validacion = "fail";
+            Fecha fecha = new Fecha();
+
+            Fase_Tiempo fase_tiempo = new Fase_Tiempo();
+            fase_tiempo.ID_FASE = t.ID_FASE;
+            fase_tiempo.FECHA_MODIFICACION = fecha.fecha();
+            fase_tiempo.USUARIO_MODIFICACION = user;
+
+            int result = dao_fase.ActualizarEstadoDeshabilitarFase(fase_tiempo);
+
+            if (result == 1)
+            {
+                validacion = "sucess";
+            }
+            return validacion;
+        }
+
+        public string Listar_Fases_Proyecto(string id)
+        {
+            List<Fase_Tiempo> fase_Tiempos = new List<Fase_Tiempo>();
+            int val = int.Parse(id);
+            fase_Tiempos = dao_fase.Listar_Fase_Tiempo_Filtrado(val);
+            string sJSONResponse = JsonConvert.SerializeObject(fase_Tiempos, Formatting.Indented);
+            return sJSONResponse;
+        }
+
+    }
+}
--- Sitsa_Proyecto/Controllers/Fase_TiempoController.cs
+++ Sitsa_Proyecto/Controllers/Fase_TiempoController.cs
@@ -1,5 +1,6 @@
 ﻿using Biblioteca_Clases.DAO;
 using Biblioteca_Clases.Models;
+using Biblioteca_Clases.Modelos;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Web.Mvc;
@@ -11,6 +12,8 @@ namespace WebApplication2.Controllers
         // GET: Fase_Tiempo
 
         Fase_TiempoDAO dao_fase = new Fase_TiempoDAO();
+        Fases_Modelo fase_modelo = new Fases_Modelo();
+
         public ActionResult Index()
         {
             return View();
@@ -19,23 +22,8 @@ namespace WebApplication2.Controllers
         [HttpPost]
         public JsonResult agregar_fase(Fase_Tiempo fase)
         {
-            var t = fase;
-            string validacion = "fail";
-            Fecha fecha = new Fecha();
+            string validacion = fase_modelo.agregar_fase(fase, (string)(Session["User"]));
 
-            Fase_Tiempo fase_tiempo = new Fase_Tiempo();
-            fase_tiempo.DESCRIPCION = t.DESCRIPCION;
-            fase_tiempo.TIEMPO = t.TIEMPO;
-            fase_tiempo.FECHA_CREACION = fecha.fecha();
-            fase_tiempo.USUARIO_CREACION = (string)(Session["User"]);
-            fase_tiempo.FK_ID_PROYECTO = t.FK_ID_PROYECTO;
-
-            //int result = dao_fase.AgregarFase_Tiempo(fase_tiempo);
-
-            //if (result == 1)
-            //{
-            //    validacion = "sucess";
-            //}
             return Json(validacion, JsonRequestBehavior.AllowGet);
         }
 
@@ -44,20 +32,7 @@ namespace WebApplication2.Controllers
         public JsonResult actualizar_estado_deshabilitar_fase_tiempo(Fase_Tiempo fase)
         {
             var t = fase;
-            string validacion = "fail";
-            Fecha fecha = new Fecha();
-
-            Fase_Tiempo fase_tiempo = new Fase_Tiempo();
-            fase_tiempo.ID_FASE = t.ID_FASE;
-            fase_tiempo.FECHA_MODIFICACION = fecha.fecha();
-            fase_tiempo.USUARIO_MODIFICACION = (string)(Session["User"]);
-
-            int result = dao_fase.ActualizarEstadoDeshabilitarFase(fase_tiempo);
-
-            if (result == 1)
-            {
-                validacion = "sucess";
-            }
+            string validacion = fase_modelo.actualizar_estado_deshabilitar_fase_tiempo(fase, (string)(Session["User"]));
             return Json(validacion, JsonRequestBehavior.AllowGet);
         }
 
@@ -67,10 +42,8 @@ namespace WebApplication2.Controllers
         [HttpPost]
         public JsonResult Listar_Fases_Proyecto(string id)
         {
-            List<Fase_Tiempo> fase_Tiempos = new List<Fase_Tiempo>();
-            int val = int.Parse(id);
-            fase_Tiempos = dao_fase.Listar_Fase_Tiempo_Filtrado(val);
-            string sJSONResponse = JsonConvert.SerializeObject(fase_Tiempos, Formatting.Indented);
+            string sJSONResponse = fase_modelo.Listar_Fases_Proyecto(id); ;
+
             return Json(sJSONResponse, JsonRequestBehavior.AllowGet);
         }
 
