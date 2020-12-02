da729ec2b6e3e3123d45eb8d9766d6dcb2131365
BrandonBarquero <brandon.barquero.berrocal@est.una.ac.cr>
2020-12-01 20:12:17 -0600

    Modelo Servicios

---
+++ Biblioteca_Clases/Modelos/ServicioModelo.cs
@@ -0,0 +1,118 @@
+﻿using Biblioteca_Clases.DAO;
+using Biblioteca_Clases.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_Clases.Modelos
+{
+   public class ServicioModelo
+    {
+
+
+        ServicioDAO dao_servicio = new ServicioDAO();
+
+        public string agregar_servicio(string desc, string user)
+        {
+            string validacion = "fail";
+
+            string descripcion = desc;
+            string Usuario_Edita = user;
+            Fecha fecha = new Fecha();
+            string dato = fecha.fecha();
+
+            Servicio serv = new Servicio(descripcion, dato, Usuario_Edita);
+
+
+            int result = dao_servicio.AgregarServicio(serv);
+
+
+            if (result == 1)
+            {
+                validacion = "sucess";
+            }
+            return validacion;
+        }
+
+        public string actualizar_servicio(int id_servicio, string descripcion, string user)
+        {
+            string validacion = "fail";
+
+            string Usuario_Edita = user;
+            Fecha fecha = new Fecha();
+            string dato = fecha.fecha();
+
+            Servicio serv = new Servicio(id_servicio, descripcion, dato, Usuario_Edita);
+
+
+            int result = dao_servicio.ActualizarServicio(serv);
+
+
+            if (result == 1)
+            {
+                validacion = "sucess";
+            }
+            return validacion;
+        }
+
+        public string actualizar_estado_deshabilitar_servicio(int id_servicio, string user)
+        {
+            string validacion = "fail";
+
+            string Usuario_Edita = user;
+            Fecha fecha = new Fecha();
+            string dato = fecha.fecha();
+
+            Servicio serv = new Servicio(id_servicio, dato, Usuario_Edita);
+
+
+            int result = dao_servicio.ActualizarEstadoDeshabilitarServicio(serv);
+
+
+            if (result == 1)
+            {
+                validacion = "sucess";
+            }
+            return validacion;
+        }
+
+        public string actualizar_estado_Habilitar_servicio(int id_servicio, string user)
+        {
+            string validacion = "fail";
+
+            string Usuario_Edita = user;
+            Fecha fecha = new Fecha();
+            string dato = fecha.fecha();
+
+            Servicio serv = new Servicio(id_servicio, dato, Usuario_Edita);
+
+
+            int result = dao_servicio.ActualizarEstadoHabilitarServicio(serv);
+
+
+            if (result == 1)
+            {
+                validacion = "sucess";
+            }
+            return validacion;
+        }
+
+        public string eliminar_servicio_cliente(string dato)
+        {
+
+            string validacion = "fail";
+
+
+            int result = dao_servicio.EliminarServicioContacto(dato);
+
+            if (result == 1)
+            {
+                validacion = "sucess";
+            }
+            return validacion;
+        }
+
+    }
+}
--- Sitsa_Proyecto/Controllers/ServicioController.cs
+++ Sitsa_Proyecto/Controllers/ServicioController.cs
@@ -1,4 +1,5 @@
 ﻿using Biblioteca_Clases.DAO;
+using Biblioteca_Clases.Modelos;
 using Biblioteca_Clases.Models;
 using System.Web.Mvc;
 
@@ -7,113 +8,42 @@ namespace WebApplication2.Controllers
     public class ServicioController : Controller
     {
         // GET: Servicio
-
-        ServicioDAO dao_servicio = new ServicioDAO();
+        ServicioModelo servicio_modelo = new ServicioModelo();
 
         public ActionResult Index()
         {
             return View();
         }
 
-        public JsonResult agregar_servicio(FormCollection formCollection)
+        public JsonResult agregar_servicio(string formCollection)
         {
-            string validacion = "fail";
-
-            string descripcion = formCollection["descripcion"];
-            string Usuario_Edita = (string)(Session["User"]);
-            Fecha fecha = new Fecha();
-            string dato = fecha.fecha();
-
-            Servicio serv = new Servicio(descripcion, dato, Usuario_Edita);
-
-
-            int result = dao_servicio.AgregarServicio(serv);
-
-
-            if (result == 1)
-            {
-                validacion = "sucess";
-            }
+            string validacion = servicio_modelo.agregar_servicio(formCollection, (string)(Session["User"]));
             return Json(validacion, JsonRequestBehavior.AllowGet);
         }
 
         public JsonResult actualizar_servicio(int id_servicio, string descripcion)
         {
-            string validacion = "fail";
-
-            string Usuario_Edita = (string)(Session["User"]);
-            Fecha fecha = new Fecha();
-            string dato = fecha.fecha();
-
-            Servicio serv = new Servicio(id_servicio, descripcion, dato, Usuario_Edita);
-
-
-            int result = dao_servicio.ActualizarServicio(serv);
-
-
-            if (result == 1)
-            {
-                validacion = "sucess";
-            }
+            string validacion = servicio_modelo.actualizar_servicio(id_servicio, descripcion, (string)(Session["User"]));
             return Json(validacion, JsonRequestBehavior.AllowGet);
         }
 
 
         public JsonResult actualizar_estado_deshabilitar_servicio(int id_servicio)
         {
-            string validacion = "fail";
-
-            string Usuario_Edita = (string)(Session["User"]);
-            Fecha fecha = new Fecha();
-            string dato = fecha.fecha();
-
-            Servicio serv = new Servicio(id_servicio, dato, Usuario_Edita);
-
-
-            int result = dao_servicio.ActualizarEstadoDeshabilitarServicio(serv);
-
-
-            if (result == 1)
-            {
-                validacion = "sucess";
-            }
+            string validacion = servicio_modelo.actualizar_estado_deshabilitar_servicio(id_servicio, (string)(Session["User"]));
             return Json(validacion, JsonRequestBehavior.AllowGet);
         }
 
         public JsonResult actualizar_estado_Habilitar_servicio(int id_servicio)
         {
-            string validacion = "fail";
-
-            string Usuario_Edita = (string)(Session["User"]);
-            Fecha fecha = new Fecha();
-            string dato = fecha.fecha();
-
-            Servicio serv = new Servicio(id_servicio, dato, Usuario_Edita);
-
-
-            int result = dao_servicio.ActualizarEstadoHabilitarServicio(serv);
-
-
-            if (result == 1)
-            {
-                validacion = "sucess";
-            }
+            string validacion = servicio_modelo.actualizar_estado_Habilitar_servicio(id_servicio, (string)(Session["User"]));
             return Json(validacion, JsonRequestBehavior.AllowGet);
         }
 
         [HttpPost]
         public JsonResult eliminar_servicio_cliente(string dato)
         {
-
-            string validacion = "fail";
-
-
-            int result = dao_servicio.EliminarServicioContacto(dato);
-
-            if (result == 1)
-            {
-                validacion = "sucess";
-            }
+            string validacion = servicio_modelo.eliminar_servicio_cliente(dato);
             return Json(validacion, JsonRequestBehavior.AllowGet);
         }
     }
